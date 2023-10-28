60b14204d82d142674c1d69614c95e208761587e
Dan Mac <danendraclever24@gmail.com>
2023-10-29 01:09:27 +0700

    not woek

---
+++ src/iris_robot_description/scripts/kicker_controll.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import threading
+
+import roslib; roslib.load_manifest('teleop_twist_keyboard')
+import rospy
+
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
+
+import sys
+from select import select
+
+if sys.platform == 'win32':
+    import msvcrt
+else:
+    import termios
+    import tty
